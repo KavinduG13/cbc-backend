8a03f0caf4f35f8a8fca7cfc1c30f7fb60eccd1c
Kavindu <kkgeeshan@gmail.com>
2025-09-07 15:14:20 +0530

    Changes from Day 10 - Day 14. Admin and User login differences added and updated .env for more secure for uploading into github😎🔒

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import jwt from "jsonwebtoken";
 import productRouter from "./routes/productRouter.js";
+import cors from "cors";
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
